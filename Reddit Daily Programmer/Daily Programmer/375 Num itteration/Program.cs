cecca791620275b4cece0348397ed6dc2924c101
Uendy <alex.beshkov98@gmail.com>
2019-11-19 11:48:21 +0200

    Adding: - 375: Num Itteration (initial)

---
+++ Reddit Daily Programmer/Daily Programmer/375 Num itteration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/\
+
+        //Challenge #375 [Easy] Print a new number by adding one to each of its digit
+
+        //Description
+        //A number is input in computer then a new no should get printed by adding one to each of its digit.
+        //If you encounter a 9, insert a 10(don't carry over, just shift things around).
+       
+
+        //For example, 998 becomes 10109.
+       
+       //Bonus
+       //This challenge is trivial to do if you map it to a string to iterate over the input, operate, and then cast it back.
+       // Instead, try doing it without casting it as a string at any point, keep it numeric(int, float if you need it) only.
+    }
+}
